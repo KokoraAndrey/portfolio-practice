fb65fe1e88c480d6314cef06fb6d30d797c130bf
ruin9r <andrey18121996@gmil.com>
2025-09-04 16:49:22 +0300

    create: burger menu + animation

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+
 const burgerButton = document.querySelector('.header__burger');
 const menu = document.querySelector('.header__menu');
+const page = document.querySelector('.page');
+const menuItem = document.querySelectorAll('.header__link')
 
 burgerButton.addEventListener('click', () => {
+  page.classList.toggle('stopScroll')
   burgerButton.classList.toggle('isActive');
   menu.classList.toggle('isOpen');
 });
 
+menuItem.forEach(el => {
+  el.addEventListener('click', () => {
+    page.classList.remove('stopScroll')
+    burgerButton.classList.remove('isActive');
+    menu.classList.remove('isOpen');
+  })
+})
